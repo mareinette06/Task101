945d355c10b25a4eb97fd3eac23be22246b3edda
mareinette06 <mareinette06@gmail.com>
2023-11-08 14:42:39 +0800

    StringMethod Challenge

---
+++ Challenges/Trial.java
@@ -0,0 +1,21 @@
+package Challenges;
+
+import java.util.Scanner;
+
+public class Trial {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+                String word = scan.nextLine();
+                scan.close();
+
+        System.out.println("Length: " + word.length());
+        System.out.println("Uppercase: " + word.toUpperCase());
+        System.out.println("Lowecase: " + word.toLowerCase());
+        System.out.println("First char: " + word.charAt(0));
+        System.out.println("Last char: " + word.charAt(word.length()-1));
+        System.out.println("Substring: " + word.substring(1, 5));
+        
+    }
+
+
+    }
